d19d6780905a56744b515753bbda8657118213d5
sagawashunsuke <shunkurin73@gmail.com>
2020-12-20 18:20:45 +0900

    ログアウト機能の実装

--- public/js/app.js
+++ public/js/app.js
@@ -2014,6 +2014,11 @@ __webpack_require__.r(__webpack_exports__);
     auth: {
       type: Object | Array
     }
+  },
+  methods: {
+    logout: function logout() {
+      document.querySelector("#logout-form").submit();
+    }
   }
 });
 
@@ -58548,7 +58553,8 @@ var render = function() {
                               "a",
                               {
                                 staticClass: "navbar-brand",
-                                attrs: { id: "header-nav__logout" }
+                                attrs: { id: "header-nav__logout" },
+                                on: { click: _vm.logout }
                               },
                               [_vm._v("ログアウト")]
                             ),
